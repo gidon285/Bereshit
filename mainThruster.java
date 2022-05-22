acc25c780ab9b0bd7a683a0761a17918d56051dd
gidon <gidonaviz1@gmail.com>
2022-05-22 14:20:30 +0300

    wipe

--- mainThruster.java
+++
@@ -1,6 +0,0 @@
-package Bereshit;
-public class mainThruster extends Thrusther {
-    mainThruster(double throust,double burn_rate){
-        super(throust, burn_rate);
-    }
-}
--- secondaryThruster.java
+++
@@ -1,6 +0,0 @@
-package Bereshit;
-public class secondaryThruster extends Thrusther {
-    secondaryThruster(double throust,double burn_rate){
-        super(throust,burn_rate);
-    }
-}
